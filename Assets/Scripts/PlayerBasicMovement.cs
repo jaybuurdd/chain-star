38d2edd61a8cc36c432133b285bac67f74c74085
jobas <jobas@gmu.edu>
2023-04-28 01:16:16 -0400

    All arial attacks implemented.

--- Assets/Scripts/PlayerBasicMovement.cs
+++ Assets/Scripts/PlayerBasicMovement.cs
@@ -17,9 +17,13 @@ public class PlayerBasicMovement : MonoBehaviour
     private bool isAttacking = false;
     bool attack = false;
     bool strikeDown = false;
+    // ground attacks
     bool jab = false;
-    bool arialStike = false;
-    bool comboStrike = false;
+    bool upStrike =  false;
+    bool duplex = false;
+    // arial attacks
+    bool arialStrike = false;
+    bool arialSlash = false;
 
     private bool canDoublePress = false;
     private float doublePressTime = 0.5f; // The time window for the double press
@@ -52,6 +56,29 @@ public class PlayerBasicMovement : MonoBehaviour
         bool inAir = !controller.m_Grounded;
         if (inAir)
         {
+            // Activate the Arial Slash animation
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                if (!attack)
+                {
+                    if (canDoublePress && Time.time <= doublePressTime)
+                    {
+                        // Double press detected, do something
+                        Debug.Log("Double press detected!");
+                    }
+                    else
+                    {
+                        attack = arialSlash = true;
+                        OnAttacking();
+
+                        canDoublePress = true;
+                        doublePressTime = Time.time + 0.5f; // Set the time window for the double press
+                    }
+                }
+                
+                attack = arialSlash = false;
+                
+            }
             // Activate the ArialSpinStrike animation
             if (Input.GetKeyDown(KeyCode.W))
             {
@@ -64,7 +91,7 @@ public class PlayerBasicMovement : MonoBehaviour
                     }
                     else
                     {
-                        attack = arialStike = true;
+                        attack = arialStrike = true;
                         OnAttacking();
 
                         canDoublePress = true;
@@ -72,7 +99,30 @@ public class PlayerBasicMovement : MonoBehaviour
                     }
                 }
                 
-                attack = arialStike = false;
+                attack = arialStrike = false;
+                
+            }
+            // Activate the Arial Strike Down animation
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!attack)
+                {
+                    if (canDoublePress && Time.time <= doublePressTime)
+                    {
+                        // Double press detected, do something
+                        Debug.Log("Double press detected!");
+                    }
+                    else
+                    {
+                        attack = strikeDown = true;
+                        OnAttacking();
+
+                        canDoublePress = true;
+                        doublePressTime = Time.time + 0.5f; // Set the time window for the double press
+                    }
+                }
+                
+                attack = strikeDown = false;
                 
             }
         }
@@ -83,10 +133,10 @@ public class PlayerBasicMovement : MonoBehaviour
             
                 if (!attack) {
                     
-                    attack = strikeDown = true;
+                    attack = upStrike = true;
                     OnAttacking();
                 }
-                attack = strikeDown = false;
+                attack = upStrike = false;
                 //animator.SetBool("isStrikeDown", true);
             } 
 
@@ -102,15 +152,15 @@ public class PlayerBasicMovement : MonoBehaviour
             } 
 
             // if combo strike
-            if(Input.GetKeyDown(KeyCode.E))
-            {
-                if(!attack) {
-                    attack = comboStrike = true;
-                    OnAttacking();
-                }
+            // if(Input.GetKeyDown(KeyCode.E))
+            // {
+            //     if(!attack) {
+            //         attack = comboStrike = true;
+            //         OnAttacking();
+            //     }
 
-                attack = comboStrike = false;
-            }
+            //     attack = comboStrike = false;
+            // }
         }
 
         // Reset the double press flag if the time window has passed
@@ -128,26 +178,41 @@ public class PlayerBasicMovement : MonoBehaviour
     public void OnAttacking ()
     {
         Debug.Log("Attacking: " + attack);
-        if(attack && strikeDown){
+        //ground attacks
+        if(attack && upStrike){
             Debug.Log("You striked!");
-            animator.SetBool("isStrikeDown", true);
+            animator.SetBool("isUpStrike", true);
         }
         if(attack && jab){
             Debug.Log("You jabbed!");
             animator.SetBool("isJab", true);
         }
-        if(attack && comboStrike){
-            Debug.Log("You combo striked!");
-            animator.SetBool("isComboStrike",true);
-        }
+        // if(attack && comboStrike){
+        //     Debug.Log("You combo striked!");
+        //     animator.SetBool("isComboStrike",true);
+        // }
 
-        // Check if the player is in the air and executing the arialStrike attack
-        //bool inAir = !controller.m_Grounded;
-        if (attack && arialStike)
+        // arial attacks
+        if (attack && arialStrike)
         {
-            Debug.Log("You arial spin striked!");
+            //Debug.Log("You arial spin striked!");
             animator.CrossFade("Player_Arial_Spin_Strike", 0.5f);
             animator.SetBool("isArialSpinStrike", true);
+            animator.SetBool("isJumping", false);
+        }
+        if (attack && arialSlash)
+        {
+            //Debug.Log("You arial slashed!");
+            animator.CrossFade("Player_Combo_Strike", 0.5f);
+            animator.SetBool("isComboStrike", true);
+            animator.SetBool("isJumping", false);
+        }
+        if (attack && strikeDown)
+        {
+            Debug.Log("You down striked!");
+            animator.CrossFade("Player_Strike_Down", 0.5f);
+            animator.SetBool("isStrikeDown", true);
+            animator.SetBool("isJumping", false);
         }
     }
 
@@ -155,21 +220,34 @@ public class PlayerBasicMovement : MonoBehaviour
     public void OnAttackEnd()
 	{
         Debug.Log("Ending Attack");
-        if(!attack && !strikeDown)
-            animator.SetBool("isStrikeDown", false);
+        if(!attack && !upStrike)
+            animator.SetBool("isUpStrike", false);
         if(!attack && !jab)
             animator.SetBool("isJab", false);
-        if(!attack && !comboStrike){
-            animator.SetBool("isComboStrike", false);
-        }
+        // if(!attack && !comboStrike){
+        //     animator.SetBool("isComboStrike", false);
+        // }
 
-        // Check if the player is in the air and executing the arialStrike attack
-        bool inAir = !controller.m_Grounded;
-        if (!attack && !arialStike)
+
+        if (!attack && !arialStrike)
         {
-            Debug.Log("Arial strike ended");
+            //Debug.Log("Arial strike ended");
             //animator.CrossFade("Player_Jumping", 0.5f);
             animator.SetBool("isArialSpinStrike", false);
+            
+        }
+        if (!attack && !arialSlash)
+        {
+            Debug.Log("Arial slash ended");
+            //animator.CrossFade("Player_Jumping", 0.5f);
+            animator.SetBool("isComboStrike", false);
+            
+        }
+        if (!attack && !arialSlash)
+        {
+            Debug.Log("Arial slash ended");
+            //animator.CrossFade("Player_Jumping", 0.5f);
+            animator.SetBool("isStrikeDown", false);
             
         }
 	}
